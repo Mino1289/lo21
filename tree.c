c0a0e95a66da1c529871d0eee8408629be0ea00d
Mino1289 <kretzvictor@orange.fr>
2022-11-17 18:25:22 +0100

    Tree: Arbre binaire

--- tree.c
+++ tree.c
@@ -1,2 +1,100 @@
 #include <tree.h>
 
+void T_print_prefix(Tree *t) {
+    if (!EMPTY(t)) {
+        D_print(ROOT(t));
+        T_print_prefix(LEFT(t));
+        T_print_prefix(RIGHT(t));
+    }
+} 
+
+void T_print_postfix(Tree *t) {
+    if (!EMPTY(t)) {
+        T_print_postfix(LEFT(t));
+        T_print_postfix(RIGHT(t));
+        D_print(ROOT(t));
+    }
+} 
+
+void T_print_infix_lr(Tree *t) {
+    if (!EMPTY(t)) {
+        T_print_infix_lr(LEFT(t));
+        D_print(ROOT(t));
+        T_print_infix_lr(RIGHT(t));
+    }
+} 
+
+void T_print_infix_rl(Tree *t) {
+    if (!EMPTY(t)) {
+        T_print_infix_rl(RIGHT(t));
+        D_print(ROOT(t));
+        T_print_infix_rl(LEFT(t));
+    }
+}
+
+void T_free(Tree *t) {
+    if (!EMPTY(t)) {
+        T_free(LEFT(t));
+        T_free(RIGHT(t));
+        D_free(&ROOT(t));
+        free(t);
+    }
+}
+
+int T_size(Tree *t) {
+    if (EMPTY(t)) {
+        return 0;
+    } else {
+        return 1 + T_size(LEFT(t)) + T_size(RIGHT(t));
+    }
+}
+
+int T_count(Tree *t, Data d) {
+    if (EMPTY(t)) {
+        return 0;
+    } else {
+        return (D_cmp(ROOT(t), d) == 0 ? 1 : 0) + T_count(LEFT(t), d) + T_count(RIGHT(t), d);
+    }
+}
+
+int T_node_count(Tree *t) {
+    if (EMPTY(t)) {
+        return 0;
+    } else {
+        return 1 + T_node_count(LEFT(t)) + T_node_count(RIGHT(t));
+    }
+}
+
+int T_leaf_count(Tree *t) {
+    if (EMPTY(t)) {
+        return 0;
+    } else if (LEAF(t)) {
+        return 1;
+    } else {
+        return T_leaf_count(LEFT(t)) + T_leaf_count(RIGHT(t));
+    }
+}
+
+bool T_contains(Tree *l, Data d) {
+    if (EMPTY(l)) {
+        return false;
+    } else {
+        return (D_cmp(ROOT(l), d) == 0) || T_contains(LEFT(l), d) || T_contains(RIGHT(l), d);
+    }
+}
+
+Tree *new(Data d) {
+    Tree *t = (Tree *) malloc(sizeof(Tree));
+    ROOT(t) = d;
+    LEFT(t) = NULL;
+    RIGHT(t) = NULL;
+    return t;
+}
+
+Tree *root(Tree *right, Tree *left, Data d) {
+    Tree *t = (Tree *) malloc(sizeof(Tree));
+    ROOT(t) = d;
+    LEFT(t) = left;
+    RIGHT(t) = right;
+    return t;
+}
--- tree.h
+++ tree.h
@@ -3,5 +3,42 @@
 
 #include <base.h>
 
+#define ROOT(t) (t->data)
+#define LEFT(t) (t->left)
+#define RIGHT(t) (t->right)
+
+#define LEAF(t) (EMPTY(LEFT(t)) && EMPTY(RIGHT(t)))
+
+typedef struct _T_elem {
+    Data data;
+    struct _T_elem *left;
+    struct _T_elem *right;
+} Tree;
+
+
+void T_print_prefix(Tree *t);
+
+void T_print_postfix(Tree *t);
+
+void T_print_infix_lr(Tree *t);
+
+void T_print_infix_rl(Tree *t);
+
+void T_free(Tree *t);
+
+int T_size(Tree *t);
+
+int T_count(Tree *t, Data d);
+
+int T_node_count(Tree *t);
+
+int T_leaf_count(Tree *t);
+
+bool T_contains(Tree *l, Data d);
+
+Tree *new(Data d);
+
+Tree *root(Tree *right, Tree *left, Data d);
+
 
 #endif // __TREE_H__
